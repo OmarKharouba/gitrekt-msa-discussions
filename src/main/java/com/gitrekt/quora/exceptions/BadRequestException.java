bed16bb09e002d207af80b9c553726272db4c1ac
TheRealImaginary <mazennnnnnnnn@gmail.com>
2019-03-03 13:24:01 +0200

    feat: Add Server Execptions
    * Add BadRequest Exception.
    * Adjust ServerException to inherit from RuntimeException.
    * Adjust NotFoundException.
    Refercens https://gitlab.com/gitrekt-msa/backlog/issues/1.

---
+++ src/main/java/com/gitrekt/quora/exceptions/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.gitrekt.quora.exceptions;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+public class BadRequestException extends ServerException {
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+
+  @Override
+  public HttpResponseStatus getCode() {
+    return HttpResponseStatus.BAD_REQUEST;
+  }
+}
--- src/main/java/com/gitrekt/quora/exceptions/ServerException.java
+++ src/main/java/com/gitrekt/quora/exceptions/ServerException.java
@@ -2,6 +2,11 @@ package com.gitrekt.quora.exceptions;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
 
-public abstract class ServerException extends Exception {
+public abstract class ServerException extends RuntimeException {
+
+  protected ServerException(String message) {
+    super(message);
+  }
+
   public abstract HttpResponseStatus getCode();
 }
