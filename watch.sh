18e9c22c47179dfbeab64e381095c73d44b88d31
TheRealImaginary <mazennnnnnnnn@gmail.com>
2019-03-03 13:13:12 +0200

    chore: Remove Arango
    * Remove Arango from `pom`.
    * Remove Arango from `docker`.
    * Remove Arango models.
    * Rename project in `pom`.
    References https://gitlab.com/gitrekt-msa/backlog/issues/1.

--- watch.sh
+++ watch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 mvn compile
-mvn exec:exec &
+mvn exec:java &
 MONITORDIR="src"
 inotifywait -m -r -e create,close_write,move,delete,modify --format '%f' "${MONITORDIR}" | while read NEWFILE
 do
     echo "${NEWFILE} Changed! Live Reloading....";
     kill -9 $(ps aux | grep 'java' | awk '{print $2}');
     mvn compile;
-    mvn exec:exec &
+    mvn exec:java &
     p=$!;
 done
