3e674b6c6412c738decede60632a3015da167807
kharouba <omarkharouba4@gmail.com>
2019-03-04 23:43:13 +0200

    feat: add discussion handler

--- src/main/java/com/gitrekt/quora/database/postgres/handlers/DiscussionsPostgresHandler.java
+++ src/main/java/com/gitrekt/quora/database/postgres/handlers/DiscussionsPostgresHandler.java
@@ -1,4 +1,27 @@
 package com.gitrekt.quora.database.postgres.handlers;
 
-public class DiscussionsPostgresHandler {
+import com.gitrekt.quora.models.Discussion;
+
+import java.util.List;
+
+public class DiscussionsPostgresHandler extends PostgresHandler<Discussion> {
+
+  DiscussionsPostgresHandler() {
+    super("Discussions", Discussion.class);
+  }
+
+  public List<Discussion> getDiscussions() {
+    return super.findAll();
+  }
+
+
+  public static void main(String[] args) {
+    // Ecxeption because of environment variable while creating the DB connection
+    // check how to fix that
+    DiscussionsPostgresHandler h = new DiscussionsPostgresHandler();
+    List<Discussion> l = h.findAll();
+    for (Discussion d : l) {
+      System.out.println(d);
+    }
+  }
 }
