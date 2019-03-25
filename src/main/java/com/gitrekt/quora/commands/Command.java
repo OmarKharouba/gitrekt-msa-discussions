6081b04709ea41ec393af94683d37ca92e167909
TheRealImaginary <mazennnnnnnnn@gmail.com>
2019-03-25 17:02:23 +0200

    feat: Add `setHandler` methods for command

--- src/main/java/com/gitrekt/quora/commands/Command.java
+++ src/main/java/com/gitrekt/quora/commands/Command.java
@@ -1,17 +1,27 @@
 package com.gitrekt.quora.commands;
 
+import com.gitrekt.quora.database.arango.handlers.ArangoHandler;
+import com.gitrekt.quora.database.postgres.handlers.PostgresHandler;
 import java.util.HashMap;
 
 public abstract class Command {
 
   private HashMap<String, String> args;
 
+  private PostgresHandler<?> postgresHandler;
+
+  private ArangoHandler<?> arangoHandler;
+
   public Command(HashMap<String, String> args) {
     this.args = args;
   }
 
-  public void setArgs(HashMap<String, String> args) {
-    this.args = args;
+  public void setPostgresHandler(PostgresHandler<?> postgresHandler) {
+    this.postgresHandler = postgresHandler;
+  }
+
+  public void setArangoHandler(ArangoHandler<?> arangoHandler) {
+    this.arangoHandler = arangoHandler;
   }
 
   public abstract Object execute();
