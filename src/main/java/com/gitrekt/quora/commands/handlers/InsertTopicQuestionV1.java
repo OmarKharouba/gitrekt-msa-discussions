e0bf9396fca3ecf1c36179249ba16665c9852fce
eladdad <ebrahiem.hassan@student.guc.edu.eg>
2019-04-02 17:26:40 +0200

    fix: add invoker and controller

--- src/main/java/com/gitrekt/quora/commands/handlers/InsertTopicQuestionV1.java
+++ src/main/java/com/gitrekt/quora/commands/handlers/InsertTopicQuestionV1.java
@@ -2,27 +2,27 @@ package com.gitrekt.quora.commands.handlers;
 
 import com.gitrekt.quora.commands.Command;
 import com.gitrekt.quora.database.postgres.PostgresConnection;
+import com.gitrekt.quora.exceptions.AuthenticationException;
 import com.gitrekt.quora.exceptions.BadRequestException;
 import com.google.gson.JsonObject;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class InsertTopicQuestionV1 extends Command {
   static String[] argsNames = {"topic_id","question_id"};
 
-  public InsertTopicQuestionV1(HashMap<String, String> args) {
+  public InsertTopicQuestionV1(HashMap<String, Object> args) {
     super(args);
   }
 
   @Override
-  public Object execute() {
-    if(!checkArguments(argsNames)) {
-      return BAD_REQUEST;
-    }
+  public Object execute() throws SQLException, BadRequestException, AuthenticationException{
+    checkArguments(argsNames);
 
-    String topicId = args.get("topic_id");
-    String questionId = args.get("question_id");
+    String topicId = (String) args.get("topic_id");
+    String questionId = (String) args.get("question_id");
     return null;
   }
 }
--- src/main/java/com/gitrekt/quora/commands/handlers/PostQuestionsV1.java
+++ src/main/java/com/gitrekt/quora/commands/handlers/PostQuestionsV1.java
@@ -2,8 +2,8 @@ package com.gitrekt.quora.commands.handlers;
 
 import com.gitrekt.quora.commands.Command;
 import com.gitrekt.quora.database.postgres.handlers.QuestionsPostgresHandler;
+import com.gitrekt.quora.exceptions.AuthenticationException;
 import com.gitrekt.quora.exceptions.BadRequestException;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.sql.SQLException;
@@ -13,58 +13,38 @@ import java.util.UUID;
 public class PostQuestionsV1 extends Command {
   static String[] argsNames = {"user_id","title"};
 
-  public PostQuestionsV1(HashMap<String, String> args) {
+  public PostQuestionsV1(HashMap<String, Object> args) {
     super(args);
   }
 
   @Override
-  public Object execute() {
+  public Object execute() throws SQLException, BadRequestException, AuthenticationException{
 
-    if(!checkArguments(argsNames)) {
-      return BAD_REQUEST;
-    }
+    checkArguments(argsNames);
 
     QuestionsPostgresHandler questionHandler;
-    try{
-      questionHandler = new QuestionsPostgresHandler();
-    }catch (Exception exc){
-      exc.printStackTrace();
-
-      questionHandler = null;
-    }
+    questionHandler = new QuestionsPostgresHandler();
 
 
-    String userId = args.get("user_id");
-    String title = args.get("title");
-    String body = args.get("body");
+    String userId = (String) args.get("user_id");
+    String title = (String) args.get("title");
+    String body = (String) args.get("body");
+    UUID questionId = UUID.randomUUID();
 
-    try {
-      UUID questionId = UUID.randomUUID();
+    questionHandler.postQuestion(questionId, UUID.fromString(userId), title, body);
 
-      questionHandler.postQuestion(questionId, UUID.fromString(userId), title, body);
+    JsonObject res = new JsonObject();
+    res.addProperty("status_code","200");
+    JsonObject resBody = new JsonObject();
+    resBody.addProperty("question_id",questionId.toString());
+    resBody.addProperty("user_id",userId);
+    resBody.addProperty("title",title);
+    resBody.addProperty("body",body);
+    resBody.addProperty("upvotes",0);
+    resBody.addProperty("subscribers",0);
 
+    res.add("body", resBody);
 
-      JsonObject res = new JsonObject();
-      res.addProperty("status_code","200");
-      JsonObject resBody = new JsonObject();
-      resBody.addProperty("question_id",questionId.toString());
-      resBody.addProperty("user_id",userId);
-      resBody.addProperty("title",title);
-      resBody.addProperty("body",body);
-      resBody.addProperty("upvotes",0);
-      resBody.addProperty("subscribers",0);
-
-      res.add("body", resBody);
-
-      return res;
-    } catch (SQLException sqlException) {
-      sqlException.printStackTrace();
-
-      return BAD_REQUEST;
-    } catch (Exception exc) {
-      exc.printStackTrace();
-
-      return INTERNAL_SERVER_ERROR;
-    }
+    return res;
   }
 }
